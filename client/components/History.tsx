bc46e10f656f716dce3b3759e52b61061e1fa80d
TEKFA Fouad <fouad.tekfa@etu.univ-lehavre.fr>
2023-11-06 03:10:51 +0100

    historique des paiements pour articles payés(checked)

--- client/components/History.tsx
+++ client/components/History.tsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { SafeAreaView, View, StyleSheet, FlatList, Text } from 'react-native';
+import format from 'date-fns/format';
+
 
 const History = () => {
   const [payments, setPayments] = useState([]);
 
   const getPayments = async () => {
     try {
-      const response = await fetch("http://192.168.1.62:8080/payments/cus_OxAC1UaHHGdw7b", {
+      const response = await fetch("http://192.168.1.62:8080/payments/cus_OxAy8ekZ5nf5Lr", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
@@ -37,21 +39,25 @@ return (
     
   <SafeAreaView style={styles.container}>
     <FlatList
-      data={payments}
+      data={payments.filter(item => item.is_checked)}
       renderItem={({ item }) => {
        // console.log(item.purchased_items);
+       const checkoutDate = new Date(item.checkout_date);
         return (
           <View style={styles.paymentItem}>
             <Text style={styles.paymentLabel}>Payment ID:</Text>
             <Text style={styles.paymentValue}>{item.id}</Text>
-
+        
             <Text style={styles.paymentLabel}>Customer ID:</Text>
             <Text style={styles.paymentValue}>{item.customer.id}</Text>
             <Text style={styles.paymentLabel}>Total payé :</Text>
             <Text style={styles.paymentValue} >{
                               item.purchased_items.map( i => i.item.price*i.amount/100)
                               .reduce( ( ac, cur ) => { return ac + cur; }, 0)
-                              }
+                              } €
+                      </Text>
+                      <Text style={styles.paymentLabel}>
+                         {`Payer le ${format(checkoutDate, 'dd/MM/yy')} à ${format(checkoutDate, 'HH:mm')}`}
                       </Text>
                       <Text style={styles.paymentLabel}>Articles : </Text>
                            {item.purchased_items.map( i => {
